670ea9ebcb61eee16365552df233e9f6c763c0af
Isaac Paul <izackp@gmail.com>
2015-03-30 13:40:31 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Page Rank Scraper
+Grabs the Google Page Rank of a url, and some alexa statistics if it can.
+
+#### Usage
+Input must have 1 argument:
+
+1.  Url (www sensitive)
+
+#### Example Result
+Output is a json with the page rank and statistics:
+`
+{
+"googlePageSuccess": True,
+"googlePageRank": 9,
+"alexaSuccess": True,
+"claimedDate": "8/1/2014 6:32:28 PM",
+"linksIn": 3804847,
+"rankDelta": 0,
+"reachRank": 1,
+"trafficRank": 1
+}
+`
---
+++ pageRankScraper/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using TAlex.SEOStats.Alexa;
+using TAlex.SEOStats.Google;
+
+namespace pageRankScraper
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			args = Sample ();
+			if (args.Length != 1) {
+				ShowError ("Invalid Amount of Parameters: Must have 1 parameters. 1: url of page to rank");
+				return;
+			}
+
+			string url = args [0];
+			url = CleanUrl (url);
+
+			GoogleResult googleStatistics = new GoogleStats().GetStats(url);
+			var alexaStatistics = new AlexaStats().GetStats(url);
+
+			List<string> entries = new List<string> ();
+			entries.Add (JSONEntry("googlePageSuccess", googleStatistics.Success));
+			if (googleStatistics.Success)
+				entries.Add (JSONEntry("googlePageRank", googleStatistics.PageRank));
+
+			entries.Add (JSONEntry("alexaSuccess", alexaStatistics.Success));
+			if (alexaStatistics.Success) {
+				entries.Add (JSONEntry("claimedDate", alexaStatistics.ClaimedDate.ToString()));
+				entries.Add (JSONEntry("linksIn", alexaStatistics.LinksIn));
+				entries.Add (JSONEntry("rankDelta", alexaStatistics.RankDelta));
+				entries.Add (JSONEntry("reachRank", alexaStatistics.ReachRank));
+				entries.Add (JSONEntry("trafficRank", alexaStatistics.TrafficRank));
+			}
+
+			string jsonContent = string.Join (JSONDelimiter, entries);
+
+			//build json
+			Console.WriteLine ("{\n" + jsonContent +"\n}");
+		}
+
+		static string JSONEntry(string key, int value) {
+			return "\"" + key + "\": " + value;
+		}
+
+		static string JSONEntry(string key, bool value) {
+			return "\"" + key + "\": " + value;
+		}
+
+		static string JSONEntry(string key, string value) {
+			return "\"" + key + "\": \"" + value + "\"";
+		}
+
+		const string JSONDelimiter = ",\n";
+
+		public static string CleanUrl(string url) {
+			if (url == null)
+				return null;
+
+			if (url.Length >= 8) {
+			
+				string httpProtocol = url.Substring (0, 7);
+				string httpsProtocol = url.Substring (0, 8);
+				if (httpProtocol != "http://" && httpsProtocol != "https://")
+					url = "http://" + url;
+			} else {
+				url = "http://" + url;
+			}
+			return url;
+		}
+
+		public static string[] Sample() {
+			const string rankingSite = "http://google.com";
+			return new string[] { rankingSite};
+		}
+
+		public static void ShowError(string error) {
+			Console.WriteLine ("{\"error\": \"" + error +"\"}");
+		}
+	}
+}
